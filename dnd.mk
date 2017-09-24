b89155f7475ec225c9871962f42507ae2298af78
root <root@death-vally.c.midyear-courage-175813.internal>
2017-09-24 09:22:23 +0000

    DND-O
    Change-Id: Ia9835ce00cf8779f0fe5ec4691cce2b76b04645d

--- dnd.mk
+++ dnd.mk
@@ -23,10 +23,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/gemini/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/dnd/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_gemini
+PRODUCT_NAME := dnd_gemini
 PRODUCT_DEVICE := gemini
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 5
